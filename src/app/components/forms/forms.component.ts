89e1e47de7b4b6735815ab721ea645b406984737
GabriellGimenes <gabrielgimenes157@gmail.com>
2024-01-31 16:11:28 -0300

    feat: saving filtered units

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -2,10 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
 import { Location } from '../../types/location.interface';
+import { FilterUnitsService } from '../../services/filter-units.service';
 
+const OPENED_HOURS = {
+  morning: {
+    first: '06',
+    last: '12'
+  },
+  afternoon: {
+    first: '12',
+    last: '18'
+  },
+  night: {
+    first: '18',
+    last: '23'
+  }
+}
 
-
-
+type HOUR_INDEXES = 'morning' | 'afternoon' | 'night';
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
@@ -16,7 +30,10 @@ export class FormsComponent implements OnInit {
   filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private unitService: GetUnitsService,
+    private filterUnitsService: FilterUnitsService) { }
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
@@ -24,18 +41,17 @@ export class FormsComponent implements OnInit {
       showClosed: true
     })
     this.unitService.getAllUnits().subscribe(data => {
-      this.results = data.locations;
-      this.filteredResults = data.locations;
+      this.results = data;
+      this.filteredResults = data;
     });
   }
 
   onSubmit(): void{
-    if(!this.formGroup.value.showClosed) {
-      this.filteredResults = this.results.filter(location => location.opened === true)
-    } else {
-      this.filteredResults = this.results;
-    }
+    let {showClosed, hour} = this.formGroup.value
+    this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour);
+    this.unitService.setFilteredUnits(this.filteredResults);
   }
+
   onClean(): void{
     this.formGroup.reset();
   }
